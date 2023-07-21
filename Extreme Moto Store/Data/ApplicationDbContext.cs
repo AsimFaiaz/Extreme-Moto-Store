adcd4da031b328df6e76e0a9a75a95849014a99d
Asim Faiaz <rasim989@gmail.com>
2023-07-21 17:25:52 +1000

    Add project files.

---
+++ Extreme Moto Store/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Extreme_Moto_Store.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Extreme_Moto_Store.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //Migrations: PMC > add-migration addCategoryToDb
+        //update-database
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)   //Shortcut: ctor tab
+        {
+            
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ Extreme Moto Store/Model/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Extreme_Moto_Store.Model
+{
+    public class Category
+    {
+        [Key]
+        public int id { get; set; } //Shortcut: prop tab
+
+        [Required]
+        public String name { get; set; }
+
+        public int displayOrder { get; set; }
+    }
+}
