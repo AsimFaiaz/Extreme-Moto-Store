25c76ae15215d5e99e0838a5ad7ab319c506a86c
Asim Faiaz <rasim989@gmail.com>
2023-07-24 09:44:14 +1000

    Delete page

--- Extreme Moto Store/Pages/Categories/Delete.cshtml.cs
+++ Extreme Moto Store/Pages/Categories/Delete.cshtml.cs
@@ -1,3 +1,5 @@
+using Extreme_Moto_Store.Data;
+using Extreme_Moto_Store.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,35 @@ namespace Extreme_Moto_Store.Pages.Categories
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+
+        public Category Category { get; set; }
+
+        public DeleteModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+
+
+        public void OnGet(int id)
+        {
+            Category = _db.Category.Find(id);
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            
+                var categoryFromDb = _db.Category.Find(Category.id);
+
+                if(categoryFromDb != null) 
+                {
+                    _db.Category.Remove(categoryFromDb);
+                    await _db.SaveChangesAsync();
+                    return RedirectToPage("Index");
+                 }
+
+            return Page();
+
         }
     }
 }
