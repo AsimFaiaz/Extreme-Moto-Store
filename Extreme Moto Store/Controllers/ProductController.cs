4c8d8dbea4821f240b413f352d1eee2a9fd5a3c8
Asim Faiaz <rasim989@gmail.com>
2023-08-06 15:01:49 +1000

    Data table completed, Database filled with related products and details

--- Extreme Moto Store/Controllers/ProductController.cs
+++ Extreme Moto Store/Controllers/ProductController.cs
@@ -11,10 +11,12 @@ namespace Extreme_Moto_Store.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment;
 
-        public ProductController(IUnitOfWork unitOfWork) 
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
         }
 
         [HttpGet]
@@ -22,6 +24,26 @@ namespace Extreme_Moto_Store.Controllers
         {
             var productList = _unitOfWork.Product.GetAll(includeProperties: "Category,ItemType"); //Case sensitive
             return Json(new { data = productList });
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
+
+            //Delete old image
+            var objImagePath = Path.Combine(_hostEnvironment.WebRootPath, objFromDb.Image.TrimStart('\\'));
+            if (System.IO.File.Exists(objImagePath))
+            {
+                System.IO.File.Delete(objImagePath);
+            }
+
+            //Remove the product 
+            _unitOfWork.Product.Remove(objFromDb);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete Successful" });
         }
     }
 }
--- Extreme Moto Store/wwwroot/js/product.js
+++ Extreme Moto Store/wwwroot/js/product.js
@@ -1,5 +1,6 @@
-﻿$(document).ready(function () {
-    $('#DT_load').DataTable({
+﻿var dataTable; //Creating a var so that it can call the reload when deleting
+$(document).ready(function () {
+    dataTable=$('#DT_load').DataTable({
         "ajax": {
             "url": "/api/Product",
             "type": "GET",
@@ -12,15 +13,15 @@
             { "data": "category.name", "width": "15%" },
             { "data": "itemType.name", "width": "15%" },
             {
-                //*Note: It is important to clear catch while using anchor tag with ajax call, Sometimes it will route you to somewhere doesn't exist
+                //Important Note: With ASP 7, It is important to clear browser catch while using anchor tag with ajax call, Sometimes it will route you to somewhere doesn't exist
 
                 "data": "id",
                 "render": function (data) {                  
                     return `<div class="w-75 btn-group" >
                             <a href="/Admin/Products/Upsert?id=${data}"  class="btn btn-success text-white mx-2">
                             <i class="bi bi-pencil-square"></i>  </a>
-                            <a href="/Admin/Products/Upsert?id=${data}"  class="btn btn-danger text-white mx-2">
-                            <i class="bi bi-trash3"></i>  </a>
+                            <a onClick=Delete('/api/Product/'+${data})  class="btn btn-danger text-white mx-2">
+                             <i class="bi bi-trash3"></i>  </a>
                             </div>`
                 },
 
@@ -30,3 +31,34 @@
         "width": "100%"
     });
 });
+
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    if (data.success) {
+                        dataTable.ajax.reload();
+                        //success notification
+                        toastr.success(data.message);
+                    }
+                    else {
+                        //failsure notification
+                        toastr.error(data.message);
+                    }
+                }
+            })
+        }
+    })
+
+}
